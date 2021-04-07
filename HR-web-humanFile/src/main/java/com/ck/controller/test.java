f13c56ff39461081d5f279c11f0df2c2005e0d8b
xuyh <2368551443@qq.com>
2021-04-07 19:18:16 +0800

    版本初始化

---
+++ HR-web-humanFile/src/main/java/com/ck/controller/test.java
@@ -0,0 +1,12 @@
+package com.ck.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class test {
+    @RequestMapping("test")
+    public String query(){
+        return "test";
+    }
+}
---
+++ HR-web-index/src/main/java/com/ck/controller/Indexcontroller.java
@@ -0,0 +1,18 @@
+package com.ck.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Indexcontroller {
+    //主页
+    @RequestMapping("/")
+    public  String logo(){
+        return "index";
+    }
+    //页面跳转
+    @RequestMapping("topage")
+    public String topage(String page){
+        return page;
+    }
+}
