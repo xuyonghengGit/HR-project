a5fa6c4c9f34b4d3107366f6afcc446abb9829a0
zhangys <1594596910@qq.com>
2021-04-10 13:59:36 +0800

    张谕盛初始化模块

--- HR-dao/src/main/java/com/ck/dao/ItestDao.java
+++ HR-dao/src/main/java/com/ck/dao/ItestDao.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface ItestDao {
     @Select("SELECT * FROM mytest")
     public List<Mytest> QueryAllIndexTest();
+
+
 }
---
+++ HR-entity/src/main/java/com/ck/entity/Engage_interview.java
@@ -0,0 +1,5 @@
+package com.ck.entity;
+
+public class Engage_interview {
+    private int ein_id;
+}
