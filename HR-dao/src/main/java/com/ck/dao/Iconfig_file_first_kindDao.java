5b68f8d121200ea10c1aaa3ecef13f1ded35992a
TangYH <2370953870@qq.com>
2021-04-12 12:04:29 +0800

    唐永海的提交实体类

--- HR-dao/src/main/java/com/ck/dao/Iconfig_file_first_kindDao.java
+++ HR-dao/src/main/java/com/ck/dao/Iconfig_file_first_kindDao.java
@@ -1,4 +1,12 @@
 package com.ck.dao;
 
+import com.ck.entity.Config_file_first_kind;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
 public interface Iconfig_file_first_kindDao {
+    //查询Iconfig_file_first_kind 一级机构设置中所有数据
+    @Select("SELECT * FROM Config_file_first_kind")
+    public List<Config_file_first_kind> queryAll();
 }
